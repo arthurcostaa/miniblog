04f645eadb7c293cba9cb3f4d8b1584679e0d49c
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2025-02-03 23:00:17 -0300

    Add base.html and index.html templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 def index():
-    return 'Hello from MiniBlog!'
+    user = {'username': 'Arthur'}
+    posts = [
+        {
+            'author': {'username': 'Arthur'},
+            'body': 'Uma vez Flamengo, sempre Flamengo!',
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Muita chuva em Natal.',
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -17,5 +17,10 @@ def client():
 def test_index_route(client):
     """Test the "/" route."""
     response = client.get('/')
+
     assert response.status_code == 200
-    assert response.text == 'Hello from MiniBlog!'
+    assert b'Hello, Arthur!' in response.data
+    assert b'Arthur says: Uma vez Flamengo, sempre Flamengo!' in response.data
+    assert b'John says: Muita chuva em Natal.' in response.data
+    assert b'<div>MiniBlog: <a href="/">Home</a></div>' in response.data
+    assert b'<title>Home - MiniBlog</title>' in response.data
